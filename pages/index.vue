f3b826d51fe97b5ddc19e240fe8f303f1c196b1f
OlgaF <o170385@yandex.com>
2022-03-30 10:27:35 +0200

    added setTimeout

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,12 @@ import axios from 'axios';
 
 <template>
   <div>
-    {{requestResult}}
+    {{this.requestResult}}
   </div>
 </template>
 
 <script>
 export default {
-
  data() {
     return {
       requestResult: null
@@ -21,9 +20,9 @@ export default {
 
   methods: {
     async getNews() {
-      this.requestResult = await this.$axios.get('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=3cGGAzhEUGTJGXrdy5TNaTrU3ctblSEK');
-      for (let i=0; i< requestResult.data.results.length;i++){
-        x += renderResult([i+1], this.requestResult.data.results[i]);
+      while(true) {
+        this.requestResult = await this.$axios.get('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=3cGGAzhEUGTJGXrdy5TNaTrU3ctblSEK');
+        await new Promise(resolve => setTimeout(resolve, 10000))
       }
     },
 
